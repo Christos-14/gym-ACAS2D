a858cf26c7a13bb95b87a252ab77c5e20f355110
Christos <christos.pantso@gmail.com>
2021-10-30 22:13:15 +0100

    Saving best model during training process using EvalCallback.

--- gym_ACAS2D/agent_main.py
+++ gym_ACAS2D/agent_main.py
@@ -1,7 +1,9 @@
 from gym_ACAS2D.settings import *
 
-from stable_baselines3.common.env_checker import check_env
 from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import CallbackList, EvalCallback
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.env_checker import check_env
 
 import random
 import time
@@ -13,35 +15,68 @@ random.seed(RANDOM_SEED)
 
 
 def simulate(pause=False):
+
+    # Folders and file names
+    model_save_path = "./models/best_model_{}".format(int(TOTAL_STEPS))
+    model_file = model_save_path + "/best_model.zip"
+    log_path = model_save_path + "/results"
+
+    # We evaluate the model 100 times during training
+    eval_frequency = max(1000, TOTAL_STEPS / 100)
+
     # Initialise the  environment
     environment = gym.make("ACAS2D-v0")
-    # Path to save/load the trained model
-    model_file = r".\models\ACAS2D_PPO_{}".format(int(TOTAL_STEPS))
-    # Create and train agent my environment
+
+    # Check environment
+    check_env(environment, warn=True, skip_render_check=True)
+
+    # Train agent or load saved model
     try:
         model = PPO.load(model_file)
         print("Model loaded from file: {}".format(model_file))
+
     except FileNotFoundError:
         t_start = time.time()
-        model = PPO('MlpPolicy', environment,
+
+        # Separate evaluation env
+        eval_env = gym.make("ACAS2D-v0")
+        eval_env = Monitor(eval_env)
+        eval_callback = EvalCallback(eval_env,
+                                     best_model_save_path=model_save_path,
+                                     log_path=log_path,
+                                     eval_freq=eval_frequency)
+        # Create the callback list
+        callback = CallbackList([eval_callback])
+
+        model = PPO('MlpPolicy',
+                    environment,
                     verbose=1,
                     seed=RANDOM_SEED,
                     tensorboard_log="./ACAS2D_PPO_tensorboard/")
-        model.learn(total_timesteps=TOTAL_STEPS, tb_log_name="run_{}".format(TOTAL_STEPS))
+
+        model.learn(total_timesteps=TOTAL_STEPS,
+                    callback=callback,
+                    tb_log_name="run_{}".format(TOTAL_STEPS))
+
+        # Save final trained model
         model.save(model_file)
         print(f"Model training complete in {(time.time() - t_start) / 60.0} minutes.")
 
-    # Test
+        # Load best model saved during training
+        model = PPO.load(model_file)
+        print("Model loaded from file: {}".format(model_file))
+
+    # Test best trained model for a number of episodes
     for episode in range(1, EPISODES + 1):
-        # At the first episode, check the environment
-        if episode == 1:
-            check_env(environment, warn=True, skip_render_check=True)
+        # # At the first episode, check the environment
+        # if episode == 1:
+        #     check_env(environment, warn=True, skip_render_check=True)
         # Reset the environment
         state = environment.reset()
         # Set game episode
         environment.game.episode = episode
-        # Episode reward
-        total_reward = 0
+        # # Episode reward
+        # total_reward = 0
         # AI tries up to MAX_TRY times
         for t in range(MAX_STEPS):
             # Quit if the game window closes
@@ -51,7 +86,7 @@ def simulate(pause=False):
             action, _states = model.predict(state)
             # Do action and get result
             next_state, reward, done, info = environment.step(action)
-            total_reward += reward
+            # total_reward += reward
             # Set up for the next iteration
             state = next_state
             # Draw games
@@ -59,7 +94,7 @@ def simulate(pause=False):
             # When episode is done, print reward
             if done:
                 print("Episode {:<3}: Time steps: {:<7} - Outcome: {:<10} - Total Reward = {}"
-                      .format(episode, t, OUTCOME_NAMES[environment.game.outcome], total_reward))
+                      .format(episode, t, OUTCOME_NAMES[environment.game.outcome], environment.game.total_reward))
                 break
             # Pause the game screen to start video capture
             if pause and episode == 0 and t == 0:
--- gym_ACAS2D/envs/game.py
+++ gym_ACAS2D/envs/game.py
@@ -346,7 +346,8 @@ class ACAS2DGame:
             self.outcome = 1
             done = True
         if done:
-            print("is_done() 	>>> Outcome: {} Total Reward: {}".format(OUTCOME_NAMES[self.outcome], self.total_reward))
+            print("is_done() 	>>> Outcome: {} Total Reward: {}".
+                  format(OUTCOME_NAMES[self.outcome].upper(), self.total_reward))
         return done
 
     def view(self):
